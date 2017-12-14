5f946127689f6bac28fc409178863ac00c56e1db
shailendra <shailendra@healint.com>
2017-12-14 16:07:39 +0800

    stub for sending email updated

--- src/main/java/services/EmailSender.java
+++ src/main/java/services/EmailSender.java
@@ -1,13 +1,15 @@
 package services;
 
+import models.EmailAddress;
+import models.Message;
+
 /**
  * Created by SKY on 12/14/2017.
  */
 public class EmailSender {
 
-	public void sendMessageToEmail()// TODO
-	{
-		
+	public void sendMessageToEmail(Message message, EmailAddress emailDestinationAddress) {
+		System.out.println("Stub for sending email - it has not been implemented");
 	}
 
 }
