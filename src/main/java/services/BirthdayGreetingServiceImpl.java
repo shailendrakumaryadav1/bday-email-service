594c93e5e26fd8d7478de4b2ee22fbb8c315e92b
shailendra <shailendra@healint.com>
2017-12-14 17:34:27 +0800

    BirthdayGreetingService implementation done

--- src/main/java/services/BirthdayGreetingServiceImpl.java
+++ src/main/java/services/BirthdayGreetingServiceImpl.java
@@ -1,12 +1,17 @@
 package services;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import models.EmailAddress;
+import models.Employee;
+import models.Message;
 import util.EmployeeRepo;
 
 /**
  * Created by SKY on 12/14/2017.
  */
 public class BirthdayGreetingServiceImpl implements BirthdayGreetingService {
-
 
 	private EmployeeRepo employeeRepo;
 	private EmailSender emailSender;
@@ -16,8 +21,19 @@ public class BirthdayGreetingServiceImpl implements BirthdayGreetingService {
 		this.emailSender = emailSender;
 	}
 
-	public void sendGreetings()
-	{
+	public void sendGreetings() {
+		List<Employee> employees = employeeRepo.findEmployeesWhoAreBornOn(LocalDate.now());
+		for (final Employee employee : employees) {
+			emailSender.sendMessageToEmail(getGreetingMessage(employee), getEmailAddress(employee));
+		}
+	}
 
+	private Message getGreetingMessage(Employee employee) {
+		return new Message("Happy Birthday!",
+				String.format("Happy birthday, dear %s!", employee.getFirstName()));
+	}
+
+	private EmailAddress getEmailAddress(Employee employee) {
+		return new EmailAddress(employee.getEmail());
 	}
 }
