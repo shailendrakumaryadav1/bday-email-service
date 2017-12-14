e666fcf264960673dcc464edc5e7238350d3f508
shailendra <shailendra@healint.com>
2017-12-14 17:05:11 +0800

    BirthdayGreetingService interface created

---
+++ src/main/java/services/BirthdayGreetingService.java
@@ -0,0 +1,10 @@
+package services;
+
+/**
+ * Created by SKY on 12/14/2017.
+ */
+public interface BirthdayGreetingService {
+
+	public void sendGreetings();
+
+}
---
+++ src/main/java/services/BirthdayGreetingServiceImpl.java
@@ -0,0 +1,13 @@
+package services;
+
+/**
+ * Created by SKY on 12/14/2017.
+ */
+public class BirthdayGreetingServiceImpl implements BirthdayGreetingService {
+
+
+	public void sendGreetings()
+	{
+
+	}
+}
