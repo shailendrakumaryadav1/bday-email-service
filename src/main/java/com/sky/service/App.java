68a0ca5917e622500442f89fb95040a90b2995ca
shailendra <shailendra@healint.com>
2017-12-14 17:44:46 +0800

    main method updated to call BirthdayGreetingService

--- src/main/java/com/sky/service/App.java
+++ src/main/java/com/sky/service/App.java
@@ -1,5 +1,14 @@
 package com.sky.service;
 
+import java.io.File;
+
+import services.BirthdayGreetingService;
+import services.BirthdayGreetingServiceImpl;
+import services.EmailSender;
+import util.EmployeeReader;
+import util.EmployeeRepo;
+import util.EmployeeRepoImpl;
+
 /**
  * Hello world!
  *
@@ -9,5 +18,14 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+		File file = new File("FILE PATH");
+
+		EmployeeReader employeeReader = new EmployeeReader();
+
+		EmployeeRepo employeeRepo = new EmployeeRepoImpl(file, employeeReader);
+		EmailSender emailSender = new EmailSender();
+		BirthdayGreetingService birthdayGreetingService = new BirthdayGreetingServiceImpl(employeeRepo, emailSender);
+		birthdayGreetingService.sendGreetings();
     }
 }
